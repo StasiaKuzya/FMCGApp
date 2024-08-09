f9543c826f241ecf3e14237720388ee624bf72be
StasiaKuzya <kuzina-sta@mail.ru>
2024-08-09 17:58:55 +0300

    Initial Commit

---
+++ FMCGApp/FMCGAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FMCGAppApp.swift
+//  FMCGApp
+//
+//  Created by Анастасия on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FMCGAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
